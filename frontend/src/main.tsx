a00382b877684e965f4f709806a61a03037d6699
moses <mosesjoseph102030@gmail.com>
2026-08-26 16:53:06 -0700

    Add frontend API URL daignostic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
